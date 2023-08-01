2021e191cb81129fe1547056865557e4e35ca732
Abk-coder <dkhadre@ept.sn>
2023-08-01 18:49:23 +0000

    Projet helloDIC2 J-2

---
+++ app/src/main/java/sn/ept/hellodic2/AcceuilActivity.java
@@ -0,0 +1,14 @@
+package sn.ept.hellodic2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class AcceuilActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
